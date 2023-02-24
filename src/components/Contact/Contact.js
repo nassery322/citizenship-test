aedaa04d236a381c2d12f50af8314cd9d43910ca
nassery322 <ahmadullahnassery322@gmail.com>
2023-02-25 01:41:11 +0300

    contact email changed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,7 @@ import './Contact.css'
 const Contact = () =>{
     return <section className='contact'id='contact'>
         <div>
-        <a href='mailto::ahmadullahnassery322@gmail.com'>info@citizenship-test.com</a>
+        <a href='mailto::ahmadullahnassery322@gmail.com'>info@citizenshipbuddy.com</a>
         </div>
         <div>&copy;Copyright 2023</div>
         <div></div>
