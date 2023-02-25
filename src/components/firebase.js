9febcad2d6f3ec46998b695e049ac493cc9d587f
nassery322 <ahmadullahnassery322@gmail.com>
2023-02-25 20:38:09 +0300

    enviromental variables  removed

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBAwCVOfjbhRSSAg1GqWwB2ZKCWV_DQTJM",
+  authDomain: "citizenshiptest-3407a.firebaseapp.com",
+  databaseURL: "https://citizenshiptest-3407a-default-rtdb.firebaseio.com",
+  projectId: "citizenshiptest-3407a",
+  storageBucket: "citizenshiptest-3407a.appspot.com",
+  messagingSenderId: "436876524126",
+  appId: "1:436876524126:web:538751f900d45ceefc52cf",
   };
   
 
